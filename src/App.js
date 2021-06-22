1992f4fd70ef97b6277b2dfc7d90801f0d990702
Michael Kang <mrmichaelkang@gmail.com>
2021-06-21 20:51:48 -0700

    Added copyright in footer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import {  BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return( 
+    <>
     <BrowserRouter>
       <Switch>
         <Route path='/weather' component={Weather}/>
         <Route path="/" component={Home}/>
       </Switch>
     </BrowserRouter>
+    <footer>
+      &copy; Copyright <a href="https://www.mrmichaelkang.com">Michael Kang</a> 2021
+      <p>Created with React.Js and Material-UI :)</p>
+    </footer>
+    </>
   )
 }
 
